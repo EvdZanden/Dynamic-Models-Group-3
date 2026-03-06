f72d82d5bee848f8cb7270170540253e9d9bd7d2
selardonia <s8n41y@gmail.com>
2026-03-06 11:09:48 +0100

    Add files via upload

---
+++ Scenario3.m
@@ -0,0 +1,37 @@
+% Scenario 3: The system shows chaotic behavior, and the lynx population peaks twice within the 200-month period. 
+% What makes this behavior chaotic and not oscillating or random? 
+% Hint: Chaotic behavior is deterministic, sensitive to initial conditions, bounded, and irregular.
+
+
+t0 = 0;                 	% start time of simulation [in months]
+tfinal = 200;                % end time of simulation [in months]
+y0 = [0.75 0.15 8];               % initial state i.e. number of tuna and sharks at t=0
+tspan = [t0 tfinal];        % time span defined by the start and end times of the simulation [in months]
+a1 = 5;                    %maximum consumption rate of plants
+a2 = 0.1;                    %maximum consumption rate of hare
+b1 = 3;                      %strength of saturation of hare
+b2 = 2;                      %strength of saturation of lynx
+d1 = 0.4;                      %per capita death rate hare
+d2 = 0.011;                      %per capita death rate lynx
+
+
+[t,y] = ode23(@(t,y) plants_hare_lynx(t,y,a1,a2,b1,b2,d1,d2), tspan, y0);
+
+% plotting the population over time
+figure("Name",'Plants-Hare-Lynx')
+plot(t,y)
+title('Plants/Hare/Lynx Populations Over Time')
+xlabel('Time [months]')
+ylabel('Population')
+legend('Plants','Hare','Lynx', 'Location','North')
+
+% Now, we are also going to plot the state space:
+% plotting the state space
+figure("Name",'State space')
+plot3(y(:,1),y(:,2), y(:,3))
+title('State space')
+xlabel('Plants Population')
+ylabel('Hare Population')
+
+zlabel('Lynx Population')
+
